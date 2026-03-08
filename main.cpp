14cecccd2e4ebcf3c04a95a70c084064d129ce21
Jesse <jesse.whiting07@gmail.com>
2026-03-08 18:56:28 -0400

    Milestone 1: Foundations review

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
+class AudioNode {
+public:
+    virtual void process() = 0;
+};
+
+class Oscillator : public AudioNode {
+public:
+    float frequency = 440.0f;
+
+    void process() override {
+        std::cout << "Oscillator running at " << frequency << " Hz" << std::endl;
+    }
+};
+
 int main() {
-    std::cout << "Hello, Amphion!" << std::endl;
+    // Here I'm using AudioNode as a pointer, as in future renditions
+    // I intend to process a list of nodes, and thus a list of nodes have to
+    // be a single type.
+    AudioNode* node = new Oscillator();
+    node->process();
     return 0;
 }
